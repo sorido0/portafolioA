83cb2b30d50446b10a89f729ca443a5ee333b39d
sorido0 <aux.asoriano@gmail.com>
2022-10-21 09:37:14 -0400

    Portafoli v0.0.1

--- typeS/main.js
+++ typeS/main.js
@@ -1,17 +1,35 @@
 console.log("mmg")
 
 
-// Swal.fire(
-//   {  
-//     icon: 'info',
-//     title: 'Aviso Importante',
-//     text: 'Esta pagina esta en construccion, para ofrecerle una mejor experiencia',
-//     showClass: {
-//         popup: 'animate__animated animate__fadeInDown'
-//       },
-//       hideClass: {
-//         popup: 'animate__animated animate__fadeOutUp'
-//       }
-//   }
-// )
+Swal.fire(
+  {  
+    icon: 'info',
+    title: 'Aviso Importante',
+    text: 'Esta pagina esta en construccion, para ofrecerle una mejor experiencia',
+    showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+  }
+)
+
+
+const btn = document.querySelector('#btn');
+btn.addEventListener('click', () => {
+    Swal.fire(
+  {  
+    icon: 'info',
+    title: 'Aviso Importante',
+    text: 'Aun estamos trabajando en esta pagina, para ofrecerle una mejor experiencia',
+    showClass: {
+        popup: 'animate__animated animate__fadeInDown'
+      },
+      hideClass: {
+        popup: 'animate__animated animate__fadeOutUp'
+      }
+  }
+)
+})
 
