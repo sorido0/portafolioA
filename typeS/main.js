17c0cee6e119dfff45dcac4b90421f27f04e9c85
sorido0 <aux.asoriano@gmail.com>
2022-10-21 08:24:02 -0400

    Portafoli v0.0.0

---
+++ typeS/main.js
@@ -0,0 +1,17 @@
+console.log("mmg")
+
+
+// Swal.fire(
+//   {  
+//     icon: 'info',
+//     title: 'Aviso Importante',
+//     text: 'Esta pagina esta en construccion, para ofrecerle una mejor experiencia',
+//     showClass: {
+//         popup: 'animate__animated animate__fadeInDown'
+//       },
+//       hideClass: {
+//         popup: 'animate__animated animate__fadeOutUp'
+//       }
+//   }
+// )
+
