0e695710d5d0659957e80f7b480b216eb3171e68
sorido0 <aux.asoriano@gmail.com>
2022-10-31 19:52:58 -0400

    Save
    Save

--- typeS/main.js
+++ typeS/main.js
@@ -1,5 +1,37 @@
 console.log("mmg")
 
+const about = document.querySelector(".aboutDiv");
+console.log(about);
+
+const blogger = document.getElementById("Blog").addEventListener("click", function(){
+  Swal.fire(
+    {  
+      icon: 'info',
+      title: 'Aviso',
+      text: 'Aun estamos en contrucción de este espacio, pronto estará disponible',
+      showClass: {
+          popup: 'animate__animated animate__fadeInDown'
+        },
+        hideClass: {
+          popup: 'animate__animated animate__fadeOutUp'
+        }
+    }
+  )
+})
+
+
+const aboutNO = document.getElementById("checkMenu").addEventListener("click", function(){
+
+  if(about.style.display === "none"){
+    about.style.display = "block";
+  }else{
+    about.style.display = "none";
+  }
+   
+})
+
+
+
 
 Swal.fire(
   {  
@@ -32,4 +64,5 @@ btn.addEventListener('click', () => {
   }
 )
 })
+
 
